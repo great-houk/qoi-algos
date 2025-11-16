19857654da827fb36e7a13f0bd18c0bc59c93fb7
Abhimanyu <agupta272@wisc.edu>
2025-11-16 10:14:57 -0600

    Writing include statements

--- gpu-cuda/qoi-cude.cu
+++ gpu-cuda/qoi-cude.cu
@@ -1,1 +1,12 @@
+// We're importing all needed modules including the header file that contains the class definitions
 #include "qoi-cude.hpp"
+// This is the CUDA runtime API for memory management function like cudeMalloc, cudaMemcpy, cudaFree
+#include <cuda_runtime.h>
+// This includes definitions for CUDA Kernel launch parameters like threadIdx, blockIdx, blockDim
+#include <device_launch_parameters.h>
+// Standard library 
+#include <cstdlib>
+// For memory manipulation functions like memcpy and memcmp
+#include <iostream>
+// For I/O operations
+#include <vector>
