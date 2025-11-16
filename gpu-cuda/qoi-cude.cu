caf53e10ecafbb8e42a95ce3cb2f2c1f89ea1dd4
Abhimanyu <agupta272@wisc.edu>
2025-11-16 10:27:53 -0600

    Defined CUDA check and other data structures

--- gpu-cuda/qoi-cude.cu
+++ gpu-cuda/qoi-cude.cu
@@ -10,3 +10,31 @@
 #include <iostream>
 // For I/O operations
 #include <vector>
+
+// Defining a macro that wraps CUDA API Calls to check for errors 
+// We're executing the CUDA function call and we're storing the returned error code
+// We're checking if the CUDA call failed (having any erorr code other than cudeSuccess)
+#define CUDA_CHECK(call) \
+    do { \
+        cudeError_t err = call; \
+        if(err != cudaSuccess) { \
+            fprint(stderr, "Cuda Error at %s:%d - %s\n",__FILE__,__LINE__, \
+            cudaGetErrorString(err)); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
+    
+typedef union {
+    struct {
+        uint8_t r, g, b, a;
+    } rgba;
+    uint32_t v;
+} qoi_rgba_t;
+typedef union {
+    struct {
+        int32_t byte_offset;
+        int32_t px_pos;
+    } fields;
+    uint8_t v[8];
+} qoi_checkpoint_t;
+static const uint8_t qoi_padding[8] = {0, 0, 0, 0, 0, 0, 0, 1};
